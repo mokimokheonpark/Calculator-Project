2ab0c49c40aeb25da16d3e9a51f12e049b18d440
mokimokheonpark <105946843+mokimokheonpark@users.noreply.github.com>
2022-11-29 16:52:07 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// main function
+
+func main() {
+
+	// main loop
+	for true {
+
+		// input from command line
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		input := scanner.Text()
+
+		// conditions of loop termination
+		if input == "exit" || input == "Exit" || input == "EXIT" {
+			break
+		}
+
+		// make a parse tree using input
+		equation, err := ParseTree(input)
+
+		// if an error exists in the parse tree, the corresponding error will be printed
+		if err != nil {
+			fmt.Println("Error:", err)
+			// otherwise, the calculated value will be printed
+		} else {
+			fmt.Println(equation.Calculate())
+		}
+
+	}
+}
